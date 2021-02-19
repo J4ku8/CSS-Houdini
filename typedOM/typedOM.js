2c8148ae1b2dc2dc3c4a87f7120b5a204b8b5dd4
jakubtichy <jakubtichy4@gmail.com>
2021-02-19 14:05:18 +0100

    [UPDATE]:TOM typo change

--- typedOM/typedOM.js
+++ typedOM/typedOM.js
@@ -4,16 +4,27 @@ function CSSOMexamples(){
     let link = document.querySelector('.link');
     let cssom = document.querySelector('#cssom');
 
-
-    console.log("Pozadí DOM elementu body: "+window.getComputedStyle(document.body).background);
-    console.log("Délka elemntů tříd 'link', které jsou použity v menu jako odkazy: "+window.getComputedStyle(link).getPropertyValue('width'));
-    console.log("Pozadí elemntu trády 'glass', který slouží jako skleněné pozadí: "+window.getComputedStyle(glass).getPropertyValue('width'));
-    console.log("Délka pseudo elementu ::before u třídy: "+window.getComputedStyle(link, '::before').width);
+    
+console.group()
+console.log('%c CSSOM', 'color: red')
+    console.log("Pozadí DOM elementu body= "+window.getComputedStyle(document.body).background);
+    console.log("Délka elemntů tříd 'link', které jsou použity v menu jako odkazy= "+window.getComputedStyle(link).getPropertyValue('width'));
+    console.log("Pozadí elemntu trády 'glass', který slouží jako skleněné pozadí= "+window.getComputedStyle(glass).getPropertyValue('width'));
+    console.log("Délka pseudo elementu ::before u třídy= "+window.getComputedStyle(link, '::before').width);
 
     //Setování vlastností pomocí CSSOM
     cssom.style.setProperty('color', 'blue');
+    console.groupEnd()
 }
 CSSOMexamples();
+
+//TOM
+function TOMChangeTextColor(){
+    let tom = document.querySelector('#tom');
+    tom.attributeStyleMap.set('color', 'red');
+    console.log(tom.attributeStyleMap.get('color'))
+}
+TOMChangeTextColor();
 
 
 CSS.registerProperty({
@@ -26,6 +37,18 @@ CSS.registerProperty({
     name:'--scale',
     syntax: '<number>',
     initialValue: '1',
+    inherits: false
+});
+CSS.registerProperty({
+    name:'--value',
+    syntax: '<number>',
+    initialValue: '0',
+    inherits: false
+});
+CSS.registerProperty({
+    name:'--width',
+    syntax: '<number>',
+    initialValue: '0',
     inherits: false
 });
 //TOM change opacity by hover
@@ -85,15 +108,57 @@ function rotateTOM(){
 }
 rotateTOM();
 
-//TODO calculate width by user input
+
+//TODO proč append nefunguje
+function changeStyle(){
+    button1=document.querySelector("#example");
+    button1.attributeStyleMap.set('opacity',  CSS.number(0));
+    setTimeout(()=>{
+        button1.attributeStyleMap.delete('opacity');
+        button1.attributeStyleMap.append('opacity-radius',CSS.number(.5))},3000)
+}
+
+
+function CSSStyleValue(){
+    const css = CSSStyleValue.parse(
+        'transform', 'translate3d(10px,10px,0) scale(0.5)');
+        console.log(css)
+}
+//CSSStyleValue();
+
+function unparsedValues(){
+//TODO proč prázdné objekty
+let values = new CSSUnparsedValue(['30', '60'])
+console.log(value.entries({i: 0, l:'1'}))
+console.log(values.keys())
+}
+
 function calcWidth(){
     const number = document.querySelector('#number');
     const fill = document.querySelector('.fill');
-    const borderWidth=fill.computedStyleMap().get('width').value;
-    number.addEventListener('input', ()=>{
-        document.getElementById("percents").innerHTML = number.value+"%";
-        number.value >100 ? number.value=100 : number.value
-        fill.attributeStyleMap.set('width', CSS.px((number.value*borderWidth)/100));
-    })
+    const bar = document.querySelector('.bar');
+
+    let value  = new CSSUnparsedValue( ['30'] );
+
+    bar.attributeStyleMap.set('width', CSS.vw(value[0]));
+    const barWidth =bar.attributeStyleMap.get('width');
+    const keyword = new CSSKeywordValue(bar.computedStyleMap().get('width').value).value;
+
+    document.getElementById("percents").innerHTML = number.value+"%";
+    number.value >100 ? number.value=100 : number.value
+    fill.attributeStyleMap.set('width', CSS.px(((number.value*keyword)*0.99)/100));
 }
-calcWidth();
+
+
+
+function keywordValue(){
+    const number = document.querySelector('#number');
+    const fill = document.querySelector('.fill');
+    const borderWidth=fill.computedStyleMap().get('width').value;
+    const keyword = new CSSKeywordValue(borderWidth);
+
+    console.log('Pomocí provolání metody value, můžeme získat pouze samoutnou hodnotu:'+keyword.value)
+    console.log('%c Níže můžete vidět instanci třídy CSSKeywordValue', 'color: red')
+    console.dir( keyword );
+}
+keywordValue();
